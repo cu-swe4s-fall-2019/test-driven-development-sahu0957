7915ddd069fbe843f9a1d5c02a6082c8bd71262e
sahu0957 <sahu0957@colorado.edu>
2019-09-27 01:39:54 +0000

    added get_data functionality, functional testing

---
+++ test_get_data.sh
@@ -0,0 +1,16 @@
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+
+. ssshtest
+
+# Test whether we exit when no stdin is there
+run read_stdin python get_data.py 0
+assert_exit_code 1
+
+# Test whether we exit when we specify a bad index value
+run read_stdin bash gen_data.sh | python get_data.py 0
+assert_exit_code 1
+
+# Test whether we have the right columns
+A=`printf "1\t5\n2\t6" | python get_data.py 0`
+run read_stdin echo $A
+assert_in_stdout '[1.0, 2.0]'
