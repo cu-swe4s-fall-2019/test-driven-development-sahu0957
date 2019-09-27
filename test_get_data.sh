96fe3144bbb9b95cbc18ca4890ac0a15899b536b
sahu0957 <sahu0957@colorado.edu>
2019-09-27 03:30:07 +0000

    Bugfixes for Travis CI testing

--- test_get_data.sh
+++ test_get_data.sh
@@ -3,11 +3,11 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 . ssshtest
 
 # Test whether we exit when no stdin is there
-run read_stdin python get_data.py 0
+run read_no_stdin python get_data.py 0
 assert_exit_code 1
 
 # Test whether we exit when we specify a bad index value
-run read_stdin bash gen_data.sh | python get_data.py 0
+run read_badindex bash gen_data.sh | python get_data.py 0
 assert_exit_code 1
 
 # Test whether we have the right columns
