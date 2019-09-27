052d4e3766cc668eb23d082dfbc59529fae578b6
sahu0957 <sahu0957@colorado.edu>
2019-09-27 02:23:50 +0000

    added histogram module, unit tests

--- data_viz.py
+++ data_viz.py
@@ -20,7 +20,20 @@ def boxplot(L, out_file_name):
         sys.exit(1)
 
 def histogram(L, out_file_name):
-    pass
+    if len(L) == 0:
+        return None
+    try:
+        width = 3
+        height = 3
+        fig = plt.figure(figsize=(width, height), dpi=300)
+        ax = fig.add_subplot(1,1,1)
+        ax.hist(L, 5)
+        plt.savefig(out_file_name,bbox_inches='tight')
+    except TypeError:
+        raise TypeError('List contains nonnumber entries!')
+    except:
+        print("An unknown exception has occured. Exiting...")
+        sys.exit(1)
 
 def combo(L, out_file_name):
     pass
--- test_data_viz.py
+++ test_data_viz.py
@@ -6,31 +6,48 @@ from os import path
 
 class TestDataViz(unittest.TestCase):
 
-    def test_data_viz_empty_list(self):
+    def test_data_viz_empty_list_boxplot(self):
         L = []
         r = data_viz.boxplot(L, 'boxplot')
         self.assertEqual(r, None)
 
-    def test_data_viz_constant(self):
+    def test_data_viz_constant_boxplot(self):
         L = [1,2,3,4,5]
         data_viz.boxplot(L, 'boxplot.png')
         self.assertTrue(path.exists("boxplot.png"))
    
-    def test_data_viz_nonnumber(self):
+    def test_data_viz_nonnumber_boxplot(self):
         L = ['foo', 'bar']
         with self.assertRaises(TypeError) as ex:
             data_viz.boxplot(L, 'boxplot.png')
 
-    def test_data_viz_float(self):
+    def test_data_viz_float_boxplot(self):
         L = [1.0, 2.2, 5.1, 3.8, 4.9]
         data_viz.boxplot(L, 'boxplot.png')
         self.assertTrue(path.exists("boxplot.png"))
+
+    def test_data_viz_empty_list_histogram(self):
+        L = []
+        r = data_viz.histogram(L, 'histogram.png')
+        self.assertEqual(r, None)
+
+    def test_data_viz_constant_histogram(self):
+        L = [1,2,3,4,5]
+        data_viz.histogram(L, 'histogram.png')
+        self.assertTrue(path.exists("histogram.png"))
+   
+    def test_data_viz_float_histogram(self):
+        L = [1.0, 2.2, 5.1, 3.8, 4.9]
+        data_viz.histogram(L, 'histogram.png')
+        self.assertTrue(path.exists("histogram.png"))
+
 
     def tearDown(self):
         if path.exists("boxplot.png"):
             os.remove("boxplot.png")
         if path.exists("histogram.png"):
-            os.remove("histogram.png")
+            print("hi there")
+            #os.remove("histogram.png")
         if path.exists("combo.png"):
             os.remove("combo.pngu")
 if __name__ == '__main__':
